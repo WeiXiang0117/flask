442478ef4b8365f2d574a5ac44956b015a9ffb98
洪偉翔 <a0978916589@gmail.com>
2025-10-20 10:48:09 +0800

    add test webpage

--- index.py
+++ index.py
@@ -5,5 +5,9 @@ app = Flask(__name__) # __name__ Flask裡的套件，代表目前執行的模組
 def home():
     return "Hello Flask"
 
+@app.route("/test") #代表我們要處理的網站路徑
+def test():
+    return "Test Flask"
+
 # if __name__ == "__main__": # 如果以主程式執行
 #     app.run() # 立刻啟動伺服器
