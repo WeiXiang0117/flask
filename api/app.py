e8530a769637ecd31c7e218c78aeffb5efba8b5f
洪偉翔 <a0978916589@gmail.com>
2025-10-20 08:41:20 +0800

    Use minimal Flask app for Vercel

--- api/app.py
+++ api/app.py
@@ -1,13 +1,9 @@
 from flask import Flask
 app = Flask(__name__) # __name__ Flask裡的套件，代表目前執行的模組，若該程式為主程式，則 name = main
 
-@app.route("/", methods=["GET"]) # @為函式的裝飾 (Decorator)：以函式為基礎，提供附加的功能
+@app.route("/") # @為函式的裝飾 (Decorator)：以函式為基礎，提供附加的功能
 def home():
-    return "Hello Flask 2"
-
-@app.route("/test") #代表我們要處理的網站路徑
-def test():
-    return "Test Flask"
+    return "Hello Flask"
 
 if __name__ == "__main__": # 如果以主程式執行
     app.run() # 立刻啟動伺服器
