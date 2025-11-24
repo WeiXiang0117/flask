1bea689b78880a5b1e9cb25c24e31bede356fa64
洪偉翔 <a0978916589@gmail.com>
2025-11-24 11:42:29 +0800

    add handleData function

--- index.py
+++ index.py
@@ -1,10 +1,19 @@
 from flask import Flask
 app = Flask(__name__) # __name__ Flask裡的套件，代表目前執行的模組，若該程式為主程式，則 name = main
 
-@app.route("/") # @為函式的裝飾 (Decorator)：以函式為基礎，提供附加的功能
-def home():
-    return "Hello Flask"
 
+# @為函式的裝飾 (Decorator)：以函式為基礎，提供附加的功能
+# 建立路徑 / 對應的處理函式
+@app.route("/") 
+def home():
+    return "Hello Flask" # 用來回應 / 的處理函式
+
+# 建立路徑 /data 對應的處理函式
+@app.route("/data")
+def handleData(): # 函式名稱沒有固定，僅需寫在裝飾器下方
+    return "My Data"
+
+# 建立路徑 /test 對應的處理函式
 @app.route("/test") #代表我們要處理的網站路徑
 def test():
     return "Test Flask"
